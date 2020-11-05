c66c72669714dd1d17ae7b8745919f06fb68a82f
Merdy <sampekmoy@gmail.com>
2020-11-05 05:03:04 +0300

    socket

--- app/src/components/messenger/Messenger.vue
+++ app/src/components/messenger/Messenger.vue
@@ -1,26 +1,62 @@
 <template>
-  <div class="chat__container">
-    <div class="chat__box chatContainerScroll">
-      <message />
+  <div class="chat__main d-flex justify-content-center">
+    <div class="chat__container w-100" v-if="socketConnected">
+        <div class="chat__box chatContainerScroll">
+          <message />
+        </div>
+        <b-form-row class="w-100">
+          <b-form-textarea
+            class="chat__input"
+            placeholder="Enter something..."
+            rows="1"
+            v-model="textMessage"
+          />
+          <b-button class="ml-2 chat__submit" @click="handleSendMessage">Отправить</b-button>
+        </b-form-row>
     </div>
-    <b-form-row class="w-100">
-      <b-form-textarea
-        class="chat__input"
-        placeholder="Enter something..."
-        rows="1"
-      />
-      <b-button class="ml-2 chat__submit">Отправить</b-button>
-    </b-form-row>
+    <b-spinner v-else
+               style="width: 3rem; height: 3rem;"
+               class="mt-4"
+    />
   </div>
 </template>
 
 <script>
 import Message from "@/components/message/Message";
+import {mapState} from 'vuex'
 
 export default {
   name: 'Messenger',
   components: {
     'message': Message
+  },
+  data() {
+    return {
+      socketConnected: false,
+      messages: [],
+      textMessage: ''
+    }
+  },
+  sockets: {
+    connect() {
+      this.socketConnected = true
+    },
+    message(msg) {
+      this.messages.push(msg)
+    }
+  },
+  methods: {
+    handleSendMessage() {
+      this.$socket.emit('message', JSON.stringify({
+        user: this.user.name,
+        message: this.textMessage
+      }))
+    }
+  },
+  computed: {
+    ...mapState({
+      user: state => state.user.data
+    })
   }
 }
 </script>
